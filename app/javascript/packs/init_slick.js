09ea0d1348c9b95bbd82c5a57fc1596f09267bc7
Tyler Sangster <tylersangster@gmail.com>
2021-05-21 14:50:25 -0400

    adding slick slider for edu section also fixed JS errors

---
+++ app/javascript/packs/init_slick.js
@@ -0,0 +1,54 @@
+// bootstrap 4 breakpoints
+const breakpoint = {
+  // small screen / phone
+  sm: 576,
+  // medium screen / tablet
+  md: 768,
+  // large screen / desktop
+  lg: 992,
+  // extra large screen / wide desktop
+  xl: 1200
+};
+console.log("custom init slick js file loaded")
+
+document.addEventListener("turbolinks:load", function() {
+  // // bootstrap 4 responsive multi column slick carousel
+$('#slick_edu').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  draggable: true,
+  pauseOnHover: false,
+  infinite: true,
+  dots: false,
+  arrows: false,
+  speed: 1000,
+  
+  mobileFirst: true,
+  
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  
+  responsive: [{
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    }
+  ]
+});
+})
