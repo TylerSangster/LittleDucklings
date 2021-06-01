23bc547d6c97bf11eb3a7ded901d94e88f5b554c
Tyler Sangster <tylersangster@gmail.com>
2021-06-01 14:26:23 -0400

    more updates

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -14,24 +14,34 @@ class PageController < ApplicationController
     @staff_pages = StaffPage.where(location: ["Little Ducklings", "All"], state: "published")
     @menu = Menu.published.first
     # @blog_posts = BlogPost.published
+    @infant_program = @centre.programs.where(title: "INFANT PROGRAM").first if @centre.present?
+    @toddler_program = @centre.programs.where(title: "TODDLER PROGRAM").first if @centre.present?
+    @prek_program = @centre.programs.where(title: "PRE-KINDERGARTEN PROGRAM").first if @centre.present?
+    @preschool_program = @centre.programs.where(title: "PRESCHOOL PROGRAM").first if @centre.present?
   end
 
   def lake
     @notices = Notice.published
     @reviews = Review.published
-    @centre = Centre.where(name: "Little Ducklings").first
+    @centre = Centre.where(name: "Little Ducklings Lake").first
     @staff_pages = StaffPage.where(location: ["Little Ducklings Lake", "All"], state: "published")
     @blog_posts = BlogPost.published
     @menu = Menu.first
+    @school_program = @centre.programs.where(title: "SCHOOL AGE PROGRAM").first if @centre.present?
+    @prek_program = @centre.programs.where(title: "PRE-KINDERGARTEN PROGRAM").first if @centre.present?
   end
 
   def pond
     @notices = Notice.published
     @reviews = Review.published
     @staff_pages = StaffPage.where(location: ["Little Ducklings Pond", "All"], state: "published")
-    @centre = Centre.where(name: "Little Ducklings").first
+    @centre = Centre.where(name: "Little Ducklings Pond").first
     @blog_posts = BlogPost.published
     @menu = Menu.first
+    @infant_program = @centre.programs.where(title: "INFANT PROGRAM").first if @centre.present?
+    @toddler_program = @centre.programs.where(title: "TODDLER PROGRAM").first if @centre.present?
+    @prek_program = @centre.programs.where(title: "PRE-KINDERGARTEN PROGRAM").first if @centre.present?
+    @preschool_program = @centre.programs.where(title: "PRESCHOOL PROGRAM").first if @centre.present?
   end
 
   def locations
